5d7d278ce5b579caf70803672340981b2213071b
lpleiku <vuducnam2711@gmail.com>
2017-06-23 15:37:57 +0700

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Demo Express on Heroku' });
+  res.render('index', { title: 'Demo Express On Heroku' });
 });
 
 module.exports = router;
