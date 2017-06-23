c19e78483de7ccbd9dbd6f1f190b0173855f8353
lpleiku <vuducnam2711@gmail.com>
2017-06-23 15:41:28 +0700

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Demo Express On Heroku' });
+  res.render('index', { title: 'Demo' });
 });
 
 module.exports = router;
